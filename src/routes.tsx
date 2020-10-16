93d1994703c2cd02ec778b303f3d2d4def6ea5c1
wild santos barreto <wildsbsrreto@gmail.com>
2020-10-16 12:00:00 -0300

    Fluxo de cadastros finalizado

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,13 +7,47 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/orphanagesMap';
 import OrphanageDetails from './pages/orphanageDetails';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
+
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+                <Screen
+                    name="OrphanagesMap"
+                    component={OrphanagesMap}
+                />
+
+                <Screen
+                    name="OrphanageDetails"
+                    component={OrphanageDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Ofanato"/>
+                    }}
+                />
+
+                <Screen
+                    name="SelectMapPosition"
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione no Mapa"/>
+                    }}
+                />
+
+                <Screen
+                    name="OrphanageData"
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os dados"/>
+                    }}
+                />
+
             </Navigator>
         </NavigationContainer>
     )
